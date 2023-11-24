de06f51e1b7676aa111d95efcb497030eb507cc0
walkerNegrão <walker.esteves.negrao@gmail.com>
2023-11-24 19:41:47 -0300

    Primeiro teste: API Working

--- src/main/java/com/walker/learningspringsecurity/controller/TestAppController.java
+++ src/main/java/com/walker/learningspringsecurity/controller/TestAppController.java
@@ -1,4 +1,14 @@
 package com.walker.learningspringsecurity.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ping")
 public class TestAppController {
+    @GetMapping
+    public String testRunApp(){
+        return "pong!";
+    }
 }
